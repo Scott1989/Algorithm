44f4446301e8d5dcdf2e360ffda66eb71149c9f5
Scott1989 <pingwenxu1989@126.com>
2021-05-29 12:06:53 +0800

    堆排序增加注释
    堆排序增加注释

--- Sort/HeapSort.cs
+++ Sort/HeapSort.cs
@@ -8,6 +8,10 @@ namespace Sort
 {
     public class HeapSort:ISort
     {
+        /// <summary>
+        /// 堆排序
+        /// 将数组构建为大顶堆，最大的元素在首位，将其移动到最后，然后继续构建大顶堆，找第二大元素
+        /// </summary>
         public void Sort(IComparable[] a)
         {
             int N = a.Length;
@@ -16,13 +20,11 @@ namespace Sort
                 //将数组[0, N-1-i]调整为大顶堆
                 HeapAdjust(a, 0, N-1-i);
 
-                //最大的元素放在a[0], 将其放置到数据结尾
+                //最大的元素放在a[0], 将其放置到数据结尾N-1-i处
                 Exch(a, 0, N-1-i);
             }
         }
 
-        
-        
         private void HeapAdjust(IComparable[] a, int i, int j)
         {
             //从数组序列的最后一个元素向前查找其父亲，比父亲大，就和父亲调整位置
