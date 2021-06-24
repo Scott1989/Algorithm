bdd62a6b3668a131969632a34e5ff44958aa95ed
Scott1989 <pingwenxu1989@126.com>
2021-06-24 23:50:52 +0800

    在深度优先遍历过程中，查找两个节点之间的路径
    在深度优先遍历过程中，查找两个节点之间的路径

--- Graph/DepthFirstSearch.cs
+++ Graph/DepthFirstSearch.cs
@@ -11,5 +11,75 @@ namespace Algorithm.Graph
     /// </summary>
     public class DepthFirstSearch
     {
+        //起始节点编号
+        private int s;
+
+        //给定图
+        private Graph g;
+
+        private bool[] marked;
+
+        private int Count { get; set; }
+        
+        /// <summary>
+        /// 图搜索算法
+        /// </summary>
+        /// <param name="g">给定的图</param>
+        /// <param name="s">给定的节点编号s</param>
+        DepthFirstSearch(Graph g, int s)
+        {
+            this.Count = 0;
+            this.g = g;
+            this.s = s;
+
+            for(int i = 0; i < g.V; i++)
+            {
+                marked[i] = false;
+            }
+
+            Dfs(g, s);
+        }
+
+        /// <summary>
+        /// 从起点s,是否存在到节点v的路径
+        /// </summary>
+        /// <param name="v"></param>
+        /// <returns></returns>
+        public bool HasPathTo(int v)
+        {
+            return marked[v];
+        }
+
+        public IEnumerable<int> PathTo(int v)
+        {
+            Stack<int> path = new Stack<int>();
+            ///存在路径，再查找访问路径
+            if (HasPathTo(v))
+            {
+
+            }
+        }
+
+
+        /// <summary>
+        /// 采用深度优先遍历算法，从起点探索所有可以达到的节点
+        /// </summary>
+        /// <param name="g"></param>
+        /// <param name="s"></param>
+        private void Dfs(Graph g, int s)
+        {
+            marked[s] = true;
+            Count++;
+
+            for(int i = 0; i < g.adj[s].Count; i++)
+            {
+                int m = g.adj[s][i];
+                if (!marked[m])
+                {
+                    Dfs(g, m);
+                }
+            }
+        }
+
     }
 }
