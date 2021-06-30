f403b0de46b39158be0b10f1eea40237fbd2394b
Scott1989 <pingwenxu1989@126.com>
2021-07-01 00:25:20 +0800

    图定义的注释改进
    图定义的注释改进

--- Graph/Graph.cs
+++ Graph/Graph.cs
@@ -4,21 +4,22 @@ using System.Collections.Generic;
 namespace Algorithm.Graph
 {
     /// <summary>
-    /// 图类定义，无向图
+    ///  无向图定义
     /// </summary>
     public class Graph
     {
         /// <summary>
-        /// 图中顶点数量
+        /// 顶点数量
         /// </summary>
         public int V { get; set; }
 
         /// <summary>
-        /// 图中边数量
+        /// 边数量
         /// </summary>
         public int E { get; set; }
 
         //用于存放边信息的邻接表
+        //adj是一个数组，每一项对应一个链表，用于存放和某个节点相连的所有其他节点列表
         public List<int>[] adj { get; set; }
 
         /// <summary>
@@ -30,7 +31,7 @@ namespace Algorithm.Graph
             this.V = V;
             this.E = 0;
 
-            //初始化邻接矩阵，每一个节点对应一个链表，用于存放相连的节点信息
+            //初始化邻接表
             adj = new List<int>[V];
             for(int i = 0; i < V; i++)
             {
@@ -40,16 +41,16 @@ namespace Algorithm.Graph
 
         /// <summary>
         /// 向图中增加一个边，边的两头节点编号是v, w
+        /// 每个边的信息保存两次
         /// </summary>
-        /// <param name="v"></param>
-        /// <param name="w"></param>
+        /// <param name="v">边的节点1</param>
+        /// <param name="w">边的节点2</param>
         public void AddEdge(int v, int w)
         {
             //节点V的链表中存放对端节点w信息
             adj[v].Add(w);
 
             //节点W的链表中存放对端节点v的信息
-            //每条边的信息存放两边
             adj[w].Add(v);
         }
 
