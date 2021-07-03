9789b9a3b64ad5e9c7335721c546a96bfe6220c3
Scott1989 <pingwenxu1989@126.com>
2021-07-03 11:20:03 +0800

    简单无向图统计类测试
    简单无向图统计类测试

---
+++ GraphTest/GraphStasticTest.cs
@@ -0,0 +1,77 @@
+﻿using Algorithm.Graph;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+
+namespace GraphTest
+{
+    class GraphStasticTest
+    {
+        private Graph g;
+
+        [SetUp]
+        public void Setup()
+        {
+            g = new Graph(13);
+
+            g.AddEdge(0, 5);
+            g.AddEdge(4, 3);
+            g.AddEdge(0, 1);
+            g.AddEdge(9, 12);
+            g.AddEdge(6, 4);
+            g.AddEdge(5, 4);
+            g.AddEdge(0, 2);
+            g.AddEdge(11, 12);
+            g.AddEdge(9, 10);
+            g.AddEdge(0, 6);
+            g.AddEdge(7, 8);
+            g.AddEdge(9, 11);
+            g.AddEdge(5, 3);
+        }
+
+        [Test]
+        public void DegreeTest()
+        {
+            Assert.IsTrue(GraphStatistics.Degree(g, 0) == 4);
+            Assert.IsTrue(GraphStatistics.Degree(g, 1) == 1);
+            Assert.IsTrue(GraphStatistics.Degree(g, 2) == 1);
+            Assert.IsTrue(GraphStatistics.Degree(g, 3) == 2);
+            Assert.IsTrue(GraphStatistics.Degree(g, 4) == 3);
+            Assert.IsTrue(GraphStatistics.Degree(g, 5) == 3);
+            Assert.IsTrue(GraphStatistics.Degree(g, 6) == 2);
+            Assert.IsTrue(GraphStatistics.Degree(g, 7) == 1);
+            Assert.IsTrue(GraphStatistics.Degree(g, 8) == 1);
+            Assert.IsTrue(GraphStatistics.Degree(g, 9) == 3);
+            Assert.IsTrue(GraphStatistics.Degree(g, 10) == 1);
+            Assert.IsTrue(GraphStatistics.Degree(g, 11) == 2);
+            Assert.IsTrue(GraphStatistics.Degree(g, 12) == 2);
+        }
+
+        [Test]
+        public void AverageDegreeTest()
+        {
+            double realValue = GraphStatistics.AverageDegree(g);
+            double expectedValue = ((double)26) / 13;
+
+            //浮点数不能直接进行比较，确定精度后再进行比较
+            int precision = 4;
+            realValue = Math.Round(realValue, precision);
+            expectedValue = Math.Round(expectedValue, precision);
+
+            Assert.IsTrue(realValue.Equals(expectedValue));
+        }
+
+
+        [Test]
+        public void  MaxDegreeTest()
+        {
+            Assert.IsTrue(GraphStatistics.MaxDegree(g) == 4);
+        }
+
+        [Test]
+        public void NumberOfSelfLoopsTest()
+        {
+            Assert.IsTrue(GraphStatistics.NumberOfSelfLoops(g) == 0);
+        }
+    }
+}
