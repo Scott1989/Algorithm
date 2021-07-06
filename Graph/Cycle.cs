eacef77fcefdfcf1d0c2259ff2bda3085fc2f864
Scott1989 <pingwenxu1989@126.com>
2021-07-06 12:51:39 +0800

    无向图环检测算法
    无向图环检测算法

--- Graph/Cycle.cs
+++ Graph/Cycle.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace Algorithm.Graph
 {
+    /// <summary>
+    /// 针对简单无向图是否有环的检测
+    /// </summary>
     public class Cycle
     {
+        private Graph g;
+
+        public Cycle(Graph g)
+        {
+            this.g = g;
+        }
+
+        public bool IsCycle()
+        {
+
+            return false;
+        }
     }
 }
