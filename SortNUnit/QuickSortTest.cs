ae1ad22e2f87bda82b801e51114fffceb9a68af5
Scott1989 <pingwenxu1989@126.com>
2021-07-17 23:41:23 +0800

    快速排序单元测试用例
    快速排序单元测试用例

--- SortNUnit/QuickSortTest.cs
+++ SortNUnit/QuickSortTest.cs
@@ -1,18 +1,30 @@
+using Algorithm;
+using Algorithm.SortSpace;
 using NUnit.Framework;
+using StdIO;
+using System.Collections.Generic;
 
 namespace SortNUnit
 {
     public class QuickSortTest
     {
+        private List<TestItem> items;
+
         [SetUp]
         public void Setup()
         {
+            string filePath = "SortData\\1WInt.txt";
+            List<string> strings = TxtWorker.ReadAllLines(filePath);
+            items = DataParser.StringsToTestItems(strings);
         }
 
         [Test]
-        public void Test1()
+        public void SortTest()
         {
-            Assert.Pass();
+            QuickSort quickSort = new QuickSort();
+            quickSort.Sort(items.ToArray());
+
+            Assert.IsTrue(quickSort.IsSorted(items.ToArray()));
         }
     }
 }
