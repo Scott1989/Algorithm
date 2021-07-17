5975e42c877613c531e627f9dd225259e127e099
Scott1989 <pingwenxu1989@126.com>
2021-07-17 23:37:19 +0800

    数据格式转换工具
    数据格式转换工具，进行整数数组和字符串数组的转换，整数数组和TestItem之间的转换。

---
+++ StdIO/DataParser.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StdIO
+{
+    /// <summary>
+    /// 负责不同数据之间的格式转换
+    /// </summary>
+    public static class DataParser
+    {
+        /// <summary>
+        /// 将整数数组转换为字符串数组
+        /// </summary>
+        /// <param name="ints"></param>
+        /// <returns></returns>
+        public static List<string> IntsToStrings(List<int> ints)
+        {
+            List<string> strings = new List<string>();
+            foreach (var data in ints)
+            {
+                string oneString = data.ToString();
+                strings.Add(oneString);
+            }
+            return strings;
+        }
+
+        /// <summary>
+        /// 将字符串数组转换为整数串数组
+        /// </summary>
+        /// <param name="ints">输入的整数字符串数组</param>
+        /// <returns>整数数组</returns>
+        public static List<int> StringsToInts(List<string> strings)
+        {
+            List<int> ints = new List<int>();
+            foreach (var data in strings)
+            {
+                int value = int.Parse(data);
+                ints.Add(value);
+            }
+            return ints;
+        }
+
+        public static List<TestItem> StringsToTestItems(List<string> strings)
+        {
+            List<TestItem> items = new List<TestItem>();
+            List<int> ints = new List<int>();
+            foreach (var data in strings)
+            {
+                int value = int.Parse(data);
+                ints.Add(value);
+            }
+
+            foreach(int oneInt in ints)
+            {
+                TestItem item = new TestItem(oneInt);
+                items.Add(item);
+            }
+
+            return items;
+        }
+
+
+    }
+}
