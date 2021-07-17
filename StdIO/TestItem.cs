6d9189e5c8ad4d63567e03a15f12bcf179c8dc91
Scott1989 <pingwenxu1989@126.com>
2021-07-17 23:37:57 +0800

    增加测试数据项类定义以及文件读写工具类
    增加测试数据项类定义以及文件读写工具类

---
+++ StdIO/TestItem.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace StdIO
+{
+    public class TestItem:IComparable
+    {
+        private int value;
+
+        public TestItem(int m)
+        {
+            value = m;
+        }
+
+        public int CompareTo(TestItem that)
+        {
+            return value.CompareTo(that.value);
+        }
+
+        public int CompareTo(object obj)
+        {
+            return value.CompareTo(((TestItem)obj).value);
+        }
+
+        public override string ToString()
+        {
+            return value.ToString();
+        }
+    }
+}
---
+++ StdIO/TxtWorker.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace StdIO
+{
+    public static class TxtWorker
+    {
+        /// <summary>
+        /// 根据文件路径，返回文件中所有行信息列表
+        /// </summary>
+        /// <param name="filePath">给定的文件路径</param>
+        /// <returns>行信息列表</returns>
+        public static List<string> ReadAllLines(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return null; 
+            }
+
+            List<string> lines = new List<string>();
+            string line;
+
+            //针对文本文件进行按行解析
+            using (StreamReader file = new StreamReader(filePath))
+            {
+                while ((line = file.ReadLine()) != null)
+                {
+                    System.Console.WriteLine(line);
+                    lines.Add(line);
+                }
+                file.Close();
+            }
+            return lines;
+        }
+
+
+      
+        /// <summary>
+        /// 将字符串内容，追加到文本文件中
+        /// </summary>
+        /// <param name="filePath">给定的文件路径</param>
+        /// <param name="line">给定的字符串信息</param>
+        public static void AppendLine(string filePath, string line)
+        {
+            using (FileStream fs = new FileStream(filePath,  FileMode.Append, FileAccess.ReadWrite))
+            {
+                //文本写入
+                StreamWriter sw = new StreamWriter(fs);
+                sw.Write(line);
+                sw.Flush();
+                sw.Close();
+            }
+        }
+
+
+
+
+        /// <summary>
+        /// 批量写入文本字符串
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <param name="lines"></param>
+        public static void AppendLines(string filePath, List<string> lines)
+        {
+            using (FileStream fs = new FileStream(filePath, FileMode.Append))
+            {
+                StreamWriter sw = new StreamWriter(fs);
+                foreach(var oneLine in lines)
+                {
+                    sw.Write(oneLine);
+                    sw.Write(Environment.NewLine);
+                }
+
+                sw.Flush();
+                sw.Close();
+            }
+        }
+    }
+}
